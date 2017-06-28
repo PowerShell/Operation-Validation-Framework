91d9cdc4014e296509f3d1feac02cd870f27181f
Asbjørn Mangabat Bach Merling <ambs@tdchosting.dk>
2017-06-28 09:02:41 +0200

    Resolve conflict

--- Modules/OperationValidation/OperationValidation.psm1
+++ Modules/OperationValidation/OperationValidation.psm1
@@ -63,33 +63,63 @@ function new-OperationValidationInfo
 # endregion
 function Get-TestFromScript
 {
-  param ( [string]$scriptPath )
-  
-  Write-Verbose -Message "$scriptPath"
+    param (
+        [parameter(Mandatory)]
+        [string]$ScriptPath
+    )
 
-  $errs = $null
-  $tok = $null
-  $AST = [System.Management.Automation.Language.Parser]::ParseInput((Get-Content -ReadCount 0 -Path $scriptPath),[ref]$tok, [ref]$errs)
+    $text = Get-Content -Path $ScriptPath -Raw
+    $tokens = $null
+    $errors = $null
+    $describes = [Management.Automation.Language.Parser]::ParseInput($text, [ref]$tokens, [ref]$errors).
+       FindAll([Func[Management.Automation.Language.Ast,bool]]{
+            param ($ast)
+            $ast.CommandElements -and
+            $ast.CommandElements[0].Value -eq 'describe'
+        }, $true) |
+        ForEach-Object {
+            
+            for ($x = 0; $x -lt $_.CommandElements.Count; $x++) 
+            {
+              #Name parameter is named
+              if ($_.CommandElements[$x] -is [System.Management.Automation.Language.CommandParameterAst] -and $_.CommandElements[$x].ParameterName -eq 'Name')
+              {
+                $describeName = $_.CommandElements[$x + 1].value
+              }
+              #if we have a string without a parameter name, return first hit. Name parameter is at position 0.
+              ElseIf (($_.CommandElements[$x] -is [System.Management.Automation.Language.StringConstantExpressionAst]) -and ($_.CommandElements[$x-1] -is [System.Management.Automation.Language.StringConstantExpressionAst]))
+              {
+                $describeName = $_.CommandElements[$x].value
+              }
+            }
 
-  #get all commandASTs, do not recurse through scriptblocks.
-  $commandAST = $AST.FindAll({$args[0] -as [System.Management.Automation.Language.CommandAst]},$false)
-  
-  foreach ($command in $commandAST)
-  {
-    for ($x = 0; $x -lt $command.CommandElements.Count; $x++) 
-    {
-      #Name parameter is named
-      if ($command.CommandElements[$x] -is [System.Management.Automation.Language.CommandParameterAst] -and $command.CommandElements[$x].ParameterName -eq 'Name')
-      {
-        return $command.CommandElements[$x + 1].value
-      }
-      #if we have a string without a parameter name, return first hit. Name parameter is at position 0.
-      ElseIf (($command.CommandElements[$x] -is [System.Management.Automation.Language.StringConstantExpressionAst]) -and ($command.CommandElements[$x-1] -is [System.Management.Automation.Language.StringConstantExpressionAst]))
-      {
-        return $command.CommandElements[$x].value
-      }
-    }
-  }
+            $item = [PSCustomObject][ordered]@{
+                Name = $describeName
+                Tags = @()
+            }
+
+            # Get any tags defined
+            $tagIndex = $_.CommandElements.IndexOf(($_.CommandElements | Where-Object ParameterName -eq 'Tag')) + 1
+            if ($tagIndex -and $tagIndex -lt $_.CommandElements.Count) {
+                $tagExtent = $_.CommandElements[$tagIndex].Extent
+
+                $tagAST = [System.Management.Automation.Language.Parser]::ParseInput($tagExtent, [ref]$null, [ref]$null)
+
+                # Try to get the tags as an array
+                $tagElements = $tagAST.FindAll({$args[0] -is [System.Management.Automation.Language.ArrayLiteralAst]}, $true)
+                if ($tagElements) {
+                    $item.Tags = $tagElements.SafeGetValue()
+                } else {
+                    # Try to get the tag as a string
+                    $tagElements = $tagAST.FindAll({$args[0] -is [System.Management.Automation.Language.StringConstantExpressionAst]}, $true)
+                    if ($tagElements) {
+                        $item.Tags = @($tagElements.SafeGetValue())
+                    }
+                }
+            }
+            $item
+        }
+    $describes
 }
 
 <#
