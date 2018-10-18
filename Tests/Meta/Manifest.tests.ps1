b66c4b5064d14b28591618e93f709e240fa869de
Brandon Olin <brandon@devblackops.io>
2018-10-17 21:23:06 -0700

    Fix regex for testing change log version

--- Tests/Meta/Manifest.tests.ps1
+++ Tests/Meta/Manifest.tests.ps1
@@ -45,7 +45,7 @@ Describe 'Module manifest' {
         $script:changelogVersion = $null
         It "has a valid version in the changelog" {
             foreach ($line in (Get-Content $changelogPath)) {
-                if ($line -match "^\D*(?<Version>(\d+\.){1,3}\d+)") {
+                if ($line -match "^##\s\[(?<Version>(\d+\.){1,3}\d+)\]") {
                     $script:changelogVersion = $matches.Version
                     break
                 }
