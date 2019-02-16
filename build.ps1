98373bba9922382307fd8192f553642d33b103d8
Brandon Olin <brandon@devblackops.io>
2019-02-15 22:53:12 -0800

    Install PSDepend with the -Force

--- build.ps1
+++ build.ps1
@@ -19,7 +19,7 @@ if ($Bootstrap.IsPresent) {
     Get-PackageProvider -Name Nuget -ForceBootstrap | Out-Null
     Set-PSRepository -Name PSGallery -InstallationPolicy Trusted
     if (-not (Get-Module -Name PSDepend -ListAvailable)) {
-        Install-module -Name PSDepend -Repository PSGallery
+        Install-module -Name PSDepend -Repository PSGallery -Scope CurrentUser -Force
     }
     Import-Module -Name PSDepend -Verbose:$false
     Invoke-PSDepend -Path './requirements.psd1' -Install -Import -Force -WarningAction SilentlyContinue
